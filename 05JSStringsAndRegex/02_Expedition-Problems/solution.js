541435befeb9f8be85d1732798c47cc6abbef8e4
Milen Nikolov <milennikolov_bg@yahoo.com>
2019-02-07 02:08:17 +0200

    Regex

--- 05JSStringsAndRegex/02_Expedition-Problems/solution.js
+++ 05JSStringsAndRegex/02_Expedition-Problems/solution.js
@@ -1,19 +1,42 @@
 function solve() {
-  let str = document.getElementById('str').value;
-  let resultSpan = document.getElementById('result');
+ let messageSeparator = document.getElementById('str').value;
+ let body = document.getElementById('text').value;
+ let resultSpan = document.getElementById('result');
 
-  let inputTokens = str.split(' ').filter(a => a !== '');
-  let messageSeparator = inputTokens.shift();
+ const regex = new RegExp(`(${messageSeparator})(.*?)(${messageSeparator})`, "g");
+ // const regex = new RegExp(`(${messageSeparator})([\\w\\s]+)(${messageSeparator})`);
+  const regexCoordinates = /(north|east)[\s\S]*?([0-9]{2})[^,]*?,[^,]*?([0-9]{6})/gi;
 
-  const regex = new RegExp(`(${messageSeparator})(\\w+)(${messageSeparator})`);
-  let regexTest = (/[A-Z][a-z]+/g);
-  let body = '';
+  let textMatch = regex.exec(body);
 
-  for (let i = 0; i < inputTokens.length; i++) {
-        body += inputTokens[i];
+  let norths = [];
+  let easts = [];
+
+ let match;
+    while(match = regexCoordinates.exec(body)) {
+        if (match[1].toLowerCase() == 'north') {
+            norths.push(match);
+        } else {
+            easts.push(match);
+        }
     }
 
-let match = regex.exec(body);
-let message = match[2];
-  console.log(message);
+  let nortCoordinates = norths.pop();
+  let eastCoordinates = easts.pop();
+
+  let message = `Message: ${textMatch[2]}`;
+
+
+  let northElement = document.createElement('p');
+  northElement.textContent = `${nortCoordinates[2]}.${nortCoordinates[3]} N`
+    let eastElement = document.createElement('p');
+  eastElement.textContent = `${eastCoordinates[2]}.${eastCoordinates[3]} E`
+    let textElement = document.createElement('p');
+  textElement.textContent = message;
+
+  resultSpan.appendChild(northElement);
+  resultSpan.appendChild(eastElement);
+  resultSpan.appendChild(textElement);
+
 }
+
--- 05JSStringsAndRegex/03_James-Bond/solution.js
+++ 05JSStringsAndRegex/03_James-Bond/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById('arr').value;
+  let inputArr = JSON.parse(input);
+  let keyword = inputArr[0];
+  let keywordToLower = keyword.toLowerCase();
+  let text = inputArr[1];
+
+  let pattern = new RegExp(`${keyword}`, "gi");
+ // let search = text.search(pattern);
+  let modifiedText = text.replace(pattern, keywordToLower);
+
+
+  console.log(keyword);
+  console.log(keywordToLower);
+  console.log(modifiedText);
+  //console.log(search);
+
+  let resultSpan = document.getElementById('result');
+
+
 }
